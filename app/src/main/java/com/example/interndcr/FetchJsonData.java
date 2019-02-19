dc9be829cfb0793e0766855b7c342fa316701dc9
Toumal Mojumder <toumalmojumder@gmail.com>
2019-02-20 01:16:38 +0600

    data fetching from json

---
+++ app/src/main/java/com/example/interndcr/FetchJsonData.java
@@ -0,0 +1,43 @@
+package com.example.interndcr;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FetchJsonData extends AsyncTask<Void,Void,Void> {
+
+    String data = "";
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL url = new URL("https://raw.githubusercontent.com/appinion-dev/intern-dcr-data/master/data.json");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while (line != null){
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        MainActivity.test.setText(data);
+    }
+}
--- app/src/main/java/com/example/interndcr/MainActivity.java
+++ app/src/main/java/com/example/interndcr/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.interndcr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+private Button submit;
+public static TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submit = findViewById(R.id.submit);
+        FetchJsonData process = new FetchJsonData();
+        process.execute();
+        test = findViewById(R.id.remarkET);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
